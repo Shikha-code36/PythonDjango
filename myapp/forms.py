fc8b49cc57e7ae35f9ca535432473ace9480888d
Shikha-code36 <pandeyshikha075@gmail.com>
2021-06-20 16:54:57 -0700

    first commit

---
+++ myapp/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from django.forms import fields
+from .models import Employee
+
+
+
+class EmployeeForm(forms.ModelForm):
+    class Meta:
+        model = Employee
+        fields = "_all_"
